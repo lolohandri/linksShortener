5d2580d61162f89506e29db754ea20dcc5d14353
DESKTOP-QNQCNJ8\sviat <sviatko228@gmail.com>
2023-05-13 14:26:22 +0300

    first commit

---
+++ LinkAPI/Repository/LinkRepository.cs
@@ -0,0 +1,34 @@
+﻿using LinkAPI.Context;
+using LinkAPI.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace LinkAPI.Repository
+{
+    public class LinkRepository : GenericRepository<Link>
+    {
+        private readonly DataContext _context;
+        public LinkRepository(DataContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public bool IsUrlExists(string url)
+        {
+            var linkCheck = _context.Links.FirstOrDefault(link => link.OriginLink == url);
+            return linkCheck != null ? true : false; 
+        }
+
+        public override IEnumerable<Link> GetAll()
+        {
+            return base.GetAll();
+        }
+        public override Link Get(long id)
+        {
+            return base.Get(id);
+        }
+        public override bool Delete(long id)
+        {
+            return base.Delete(id);
+        }
+    }
+}
---
+++ LinkAPI/Utils/UrlShortener.cs
@@ -0,0 +1,19 @@
+﻿using LinkAPI.Interfaces;
+
+namespace LinkAPI.Utils
+{
+    public class UrlShortener : IUrlShortener
+    {
+        public string GetShortUrl(HttpContext ctx)
+        {
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@az";
+            var randomStr = new string(Enumerable.Repeat(chars, 8)
+                .Select(str => str[random.Next(str.Length)]).ToArray());
+
+            var resultStr = $"{ctx.Request.Scheme}://{ctx.Request.Host}/{randomStr}";
+
+            return resultStr;
+        }
+    }
+}
